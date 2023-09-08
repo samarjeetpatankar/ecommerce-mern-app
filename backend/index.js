28e1fbbea670b1a6e045fd59f621899be53e2ba8
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-08 11:44:15 +0530

    backend setup

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
+import authRoutes from "./routes/authRoute.js";
 import connectDB from "./config/db.js";
 
 // Configure environment variables
@@ -18,6 +19,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
 
 // REST API
 app.get("/", (req, res) => {
