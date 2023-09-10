61e83b24aad0542f1d494c9fb999ed8355b0d23f
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-10 12:18:54 +0530

    homepage and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
-
-const App = () => {
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import Pagenotfound from "./pages/Pagenotfound";
+// import Register from "./pages/Auth/Register";
+// import Login from "./pages/Auth/Login";
+function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} /> */}
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
