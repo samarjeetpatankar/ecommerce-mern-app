e46ccd2f438a489261ea5cdc7e5b78a29900c98c
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-11 17:20:05 +0530

    product api

--- backend/index.js
+++ backend/index.js
@@ -2,21 +2,22 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import cors from "cors";
-import authRoutes from "./routes/authRoute.js";
 import connectDB from "./config/db.js";
-import categoryRoutes from "./routes/categoryRoutes.js"
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
 
-// Configure environment variables
+//configure env
 dotenv.config();
 
 //databse config
 connectDB();
 
-// Create Express app
+//rest object
 const app = express();
 
-// Middleware
+//middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
@@ -24,13 +25,14 @@ app.use(morgan("dev"));
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
-// REST API
+//rest api
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to ecommerce app</h1>");
 });
 
-// Define the PORT
+//PORT
 const PORT = process.env.PORT || 8080;
 
 // Start the server
