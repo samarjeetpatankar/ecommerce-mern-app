58ff900d832aff0ec413f0c8010d2e1258df1105
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-12 19:12:20 +0530

    css change

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -110,7 +110,7 @@ const HomePage = () => {
     }
   };
   return (
-    <Layout title={"ALl Products - Best offers "}>
+    <Layout title={"All Products - Best offers "}>
       {/* banner image */}
       <div className="carousel-container">
         <Carousel
