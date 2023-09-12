2581c3e8fdfecdb5aa41b6575a8202c37ffe2fcd
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-12 11:34:34 +0530

    category filter and homepage

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,6 +4,9 @@ import {
   deleteProductController,
   getProductController,
   getSingleProductController,
+  productCountController,
+  productFiltersController,
+  productListController,
   productPhotoController,
   updateProductController,
 } from "../controllers/productController.js";
@@ -39,9 +42,15 @@ router.get("/get-product/:slug", getSingleProductController);
 router.get("/product-photo/:pid", productPhotoController);
 
 //delete rproduct
-router.delete("/product/:pid", deleteProductController);
+router.delete("/delete-product/:pid", deleteProductController);
 
-export default router;
+//filter product
+router.post("/product-filters", productFiltersController);
 
+//product count
+router.get("/product-count", productCountController);
 
+//product per page
+router.get("/product-list/:page", productListController);
 
+export default router; 
