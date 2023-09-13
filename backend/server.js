5c38cd69a9608ec14b456151a3ac4f5ed354faf3
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-13 13:35:39 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,12 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from 'url'; // Add this import
+
+// Get the directory name of the current module file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //configure env
 dotenv.config();
@@ -26,7 +32,11 @@ app.use(morgan("dev"));
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 
 //rest api
 app.get("/", (req, res) => {
