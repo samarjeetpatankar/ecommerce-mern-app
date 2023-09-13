45cd178793bf45239a0707c213256c95167b17da
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-13 13:54:45 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -7,50 +7,44 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from 'url'; // Add this import
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 // Get the directory name of the current module file
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-//configure env
+// Configure env
 dotenv.config();
 
-//databse config
+// Database config
 connectDB();
 
-//rest object
+// Create express app
 const app = express();
 
-//middelwares
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-//routes
+// Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-});
+// Specify the root path for serving static files
+app.use(express.static(path.join(__dirname, "public")));
 
-//rest api
+// Rest API
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to ecommerce app</h1>");
 });
 
-//PORT
+// PORT
 const PORT = process.env.PORT || 8080;
-
 
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}`);
 });
-
-
-    
