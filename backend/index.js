01ba24d8379d4a858db7e2a007b397ad3de632fc
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-13 14:11:30 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
@@ -19,7 +18,6 @@ const app = express();
 //middelwares
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
