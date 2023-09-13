ef08535f35b7a8cc9816cea35401c0cf432f7c21
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-13 14:29:59 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,12 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import { fileURLToPath } from 'url'; // Import the 'fileURLToPath' function
+import path from "path";
+
+// Get the current module's directory path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //configure env
 dotenv.config();
@@ -24,6 +30,9 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
+
 //rest api
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to ecommerce app</h1>");
@@ -32,11 +41,7 @@ app.get("/", (req, res) => {
 //PORT
 const PORT = process.env.PORT || 8080;
 
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}`);
 });
-
-
-    
