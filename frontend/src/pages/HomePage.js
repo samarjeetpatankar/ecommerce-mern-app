0b2bd8896888aeae91980e7d16f17c47aec5b59d
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-13 14:24:53 +0530

    update

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -198,7 +198,7 @@ const HomePage = () => {
             {products?.map((p) => (
               <div className="card m-2" key={p._id}>
                 <img
-                  src={`/api/v1/product/product-photo/${p._id}`}
+                  src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
                   className="card-img-top"
                   alt={p.name}
                 />
