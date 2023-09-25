2ed7dd3eb39b58c3aa62baaf17ee40be559c13f9
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-25 11:53:26 +0530

    update

--- frontend/src/pages/ProductDetails.js
+++ frontend/src/pages/ProductDetails.js
@@ -42,7 +42,7 @@ const ProductDetails = () => {
   };
   return (
     <Layout>
-      <div className="row container product-details">
+      <div className="row container product-details my-3">
         <div className="col-md-6">
           <img
             src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${product._id}`}
@@ -52,21 +52,28 @@ const ProductDetails = () => {
             width={"350px"}
           />
         </div>
-        <div className="col-md-6 product-details-info">
+        <div
+          className="col-md-6 product-details-info"
+          style={{ marginTop: "-120px" }}
+        >
           <h1 className="text-center">Product Details</h1>
           <hr />
-          <h6>Name : {product.name}</h6>
-          <h6>Description : {product.description}</h6>
+          <h6>Name: {product.name}</h6>
+          <h6>Description: {product.description}</h6>
           <h6>
-            Price :
+            Price:
             {product?.price?.toLocaleString("en-IN", {
               style: "currency",
               currency: "INR",
             })}
           </h6>
-          <h6>Category : {product?.category?.name}</h6>
+          <h6>Category: {product?.category?.name}</h6>
           <button
-            className="btn btn-dark ms-1"
+            className="btn btn-primary ms-1"
+            style={{
+              borderRadius: "50px",
+              boxShadow: "2px 2px 4px rgba(0, 0, 0, 0.2)",
+            }}
             onClick={() => {
               setCart([...cart, product]);
               localStorage.setItem("cart", JSON.stringify([...cart, product]));
@@ -77,6 +84,7 @@ const ProductDetails = () => {
           </button>
         </div>
       </div>
+
       <hr />
       <div className="row container similar-products">
         <h4>Similar Products ➡️</h4>
@@ -106,13 +114,13 @@ const ProductDetails = () => {
                 </p>
                 <div className="card-name-price">
                   <button
-                    className="btn btn-info ms-1"
+                    className="btn btn-primary ms-1"
                     onClick={() => navigate(`/product/${product.slug}`)}
                   >
                     More Details
                   </button>
                   <button
-                    className="btn btn-dark ms-1"
+                    className="btn btn-success ms-1"
                     onClick={() => {
                       setCart([...cart, product]);
                       localStorage.setItem(
